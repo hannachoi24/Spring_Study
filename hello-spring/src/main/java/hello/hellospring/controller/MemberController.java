c38f01d8d3ef5e6ef8e563b5b12bba29f503b24e
choihanna <hnc24@naver.com>
2021-07-19 21:47:55 +0900

    스프링 웹 개발 기초_MVC와 Template Engine & API

---
+++ hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
@@ -0,0 +1,7 @@
+package hello.hellospring.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MemberController { // Controller 어노테이션을 보고 spring이 뜰 때 MemberController 객체를 생성해 넣어줌 그리고 spring이 관리함
+}
