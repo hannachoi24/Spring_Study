45ba9c27269d66d591f0194f0f5b48878de7a44d
choihanna <hnc24@naver.com>
2021-07-19 21:51:13 +0900

    회원 도메인과 리포지토리 만들기

--- hello-spring/src/main/java/hello/hellospring/repository/MemberRepository.java
+++ hello-spring/src/main/java/hello/hellospring/repository/MemberRepository.java
@@ -6,8 +6,9 @@ import java.util.List;
 import java.util.Optional;
 
 public interface MemberRepository {
-    Member save(Member member); // 회원 저장
-    Optional<Member> findById(Long id); // id로 회원을 찾기
+    Member save(Member member); // 회원을 저장하면 저장된 회원 반환
+    // Optional은 만약 데이터를 받아 올 때, NULL이 올 수도 있다. 이때 Optional로 감싸서 반환한다.
+    Optional<Member> findById(Long id); // id로 회원 찾기
     Optional<Member> findByName(String name);
     List<Member> findAll(); // 지금 까지 저장된 모든 회원들의 정보를 반환
 }
