f646fdc69c53ab417cd9ba3a77911b3174325f97
choihanna <hnc24@naver.com>
2021-08-02 19:48:29 +0900

    ApplicationTest

--- hello-spring/src/test/java/hello/hellospring/HelloSpringApplicationTests.java
+++ hello-spring/src/test/java/hello/hellospring/HelloSpringApplicationTests.java
@@ -2,6 +2,8 @@ package hello.hellospring;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+
 
 @SpringBootTest
 class HelloSpringApplicationTests {
