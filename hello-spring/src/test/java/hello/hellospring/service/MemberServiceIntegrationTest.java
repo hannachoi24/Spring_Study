b5536f67e6697934286c9c06f5b2c6a58d22ff3d
choihanna <hnc24@naver.com>
2021-08-02 20:20:57 +0900

    스프링 통합 테스트

---
+++ hello-spring/src/test/java/hello/hellospring/service/MemberServiceIntegrationTest.java
@@ -0,0 +1,60 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+// import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional  //테스트를 실행할 때 트랜잭션을 실행하고 DB에 데이터를 insert 쿼리로 넣은 다음에 테스트가 끝나면 롤백해준다.
+        // 테스트를 위해 DB에 넣었던 데이터를 깔끔하게 반영 안 되게끔 해준다.
+        // 따라서 다음 테스트를 반복해서 사용할 수 있다.
+class MemberServiceIntegrationTest {
+
+    @Autowired MemberService memberService;
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    void 회원가입() {
+        //given
+        Member member = new Member();
+        member.setName("spring");
+
+        //when
+        Long saveId = memberService.join(member);
+
+        //then
+        // option+Cmd+V 하면 Optional 포맷이 만들어짐
+        Member findMember = memberService.findOne(saveId).get();
+        // option + Enter -> static으로 만들 수 있다.
+        assertThat(member.getName()).isEqualTo(findMember.getName());
+
+    }
+
+    @Test
+    public void 중복_회원_예외() {
+        //given
+        Member member1 = new Member();
+        member1.setName("spring");
+
+        Member member2 = new Member();
+        member2.setName("spring");
+
+        //when
+        memberService.join(member1);
+        IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+
+        assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+    }
+
+}
