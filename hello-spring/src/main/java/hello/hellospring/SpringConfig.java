cc414ea085c2e19a8b4736714fc0a329a36d94e5
choihanna <hnc24@naver.com>
2021-08-02 19:52:06 +0900

    고전 방식의 JDBC 활용법

--- hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -1,13 +1,25 @@
 package hello.hellospring;
 
+import hello.hellospring.repository.JdbcMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+    
+    // 스프링이 application.properties파일에 있는 정보들을 보고 자체적으로 빈을 생성 (db에 연결할 수 있는, 정보가 있는 빈을 생성)
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    } // 주입
 
     // Spring이 스프링 빈에 등록하라는 것으로 인식
     @Bean
@@ -17,7 +29,9 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() { // 인터페이스(MemberRepository), 구현체(MemoryMemberRepository)
-        return new MemoryMemberRepository(); // 인터페이스에는 new가 안됨
+
+        //return new MemoryMemberRepository(); // 인터페이스에는 new가 안됨
+        return new JdbcMemberRepository(dataSource);
     }
 }
 
