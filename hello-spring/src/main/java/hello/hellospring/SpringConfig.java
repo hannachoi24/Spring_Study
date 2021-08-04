473769e8cc878db5f506526dd564f446a74a90c7
choihanna <hnc24@naver.com>
2021-08-04 15:30:05 +0900

    JPA로 DB 연동

--- hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -1,5 +1,6 @@
 package hello.hellospring;
 
+import hello.hellospring.domain.Member;
 import hello.hellospring.repository.JdbcMemberRepository;
 import hello.hellospring.repository.JpaMemberRepository;
 import hello.hellospring.repository.MemberRepository;
@@ -12,10 +13,12 @@ import org.springframework.context.annotation.Configuration;
 import javax.persistence.EntityManager;
 import javax.sql.DataSource;
 
+// 직접 스프링 빈 작성
 @Configuration
 public class SpringConfig {
 
     /*
+    JDBC 방식
     스프링이 application.properties파일에 있는 정보들을 보고 자체적으로 빈을 생성 (db에 연결할 수 있는, 정보가 있는 빈을 생성)
     private DataSource dataSource;
     @Autowired  //DataSource를 spring에서 제공해준다
@@ -24,27 +27,44 @@ public class SpringConfig {
     }
 */
 
+   /*
+    JPA 방식
     private EntityManager em;
 
     @Autowired
     public SpringConfig(EntityManager em) {
         this.em = em;
-    }
+    }*/
+
+    // Spring Data JPA 방식
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    } // 주입을 받아서
+
+    /**
+     * [Spring Data JPA]
+     * Spring Container에서 MemberRepository를 찾는다.
+     * SpringDataJpaMemberRepository과 같은 인터페이스만 만들어놓으면 Spring Data JPA가 구현체를 만들어낸다.
+     * 그리고 Spring Bean에 등록한다.
+     */
 
     // Spring이 스프링 빈에 등록하라는 것으로 인식
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository()); // memberService를 이 부분 호출해서 스프링 빈에 등록을 해준다.
-    }
+        return new MemberService(memberRepository); // memberService를 이 부분 호출해서 스프링 빈에 등록을 해준다.
+    } // 주입받은 걸 등록해줌
 
-    @Bean
-    public MemberRepository memberRepository() { // 인터페이스(MemberRepository), 구현체(MemoryMemberRepository)
+    // @Bean
+    // public MemberRepository memberRepository() { // 인터페이스(MemberRepository), 구현체(MemoryMemberRepository)
 
         // return new MemoryMemberRepository(); // 인터페이스에는 new가 안됨
         // return new JdbcMemberRepository(dataSource);
         //return new JdbcTemplateRepository(dataSource);
-        return new JpaMemberRepository(em);
-    }
+        // return new JpaMemberRepository(em);
+    // }
 }
 
 /* Spring이 memberService와 memberRepository를 스프링 빈에 등록해준다.
--- hello-spring/src/main/java/hello/hellospring/repository/SpringDataJpaMemberRepository.java
+++ hello-spring/src/main/java/hello/hellospring/repository/SpringDataJpaMemberRepository.java
@@ -1,2 +1,21 @@
-package hello.hellospring.repository;public interface SpringDataJpaMemberRepository {
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface SpringDataJpaMemberRepository extends JpaRepository<Member, Long>, MemberRepository {
+
+    @Override
+    Optional<Member> findByName(String name);
+
+    // JpaRepository를 받고 있으면 SpringDataJpaMemberRepository이 구현체를 자동으로 만들어준다.
+    // 즉, Spring Bean에 자동으로 등록한다.
 }
+
+/**
+ * 스프링 데이터 JPA가 SpringDataJpaMemberRepository를 스프링 빈으로 자동 등록해준다.
+ * JpaRepository에서 기본적인 메소드를 다 제공해준다. save, findById 등.. 기본적인 CRUD를 제공해준다.
+ * 비즈니스에 따라 name, address, email 등 찾고 싶은 데이터가 매우 다양하다. 이런것들은 공통으로 제공하기가 어렵다.
+ */
